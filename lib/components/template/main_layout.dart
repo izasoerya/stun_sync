77647ecefc6900ec475f0f5f95faca369e66dea8
izasoerya <ihza.surya@gmail.com>
2024-05-08 01:37:11 +0700

    chore: Update bottom navigation bar in MainLayout
    This commit updates the bottom navigation bar in the MainLayout component. It adds icons and labels for the "Statistic", "Information", and "Chat" tabs. It also adds logic to handle tab selection and navigation using the PageRouter.

--- lib/components/template/main_layout.dart
+++ lib/components/template/main_layout.dart
@@ -1,32 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stun_sync/router/page_router.dart';
 
-class MainLayout extends StatelessWidget {
+final _selectedIndex = StateProvider<int>((ref) => 0);
+
+class MainLayout extends ConsumerWidget {
   const MainLayout({super.key, required this.child});
   final Widget child;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-        body: child,
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+      body: child,
+      bottomNavigationBar: Stack(
+        children: [
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Color.fromRGBO(34, 87, 122, 1),
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.table_chart_outlined),
+                label: 'Statistic',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.info_outline),
+                label: 'Information',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat_outlined),
+                label: 'Chat',
+              ),
+            ],
+            currentIndex: ref.watch(_selectedIndex),
+            onTap: (index) {
+              ref.read(_selectedIndex.notifier).state = index;
+              if (index == 0) {
+                PageRouter.router.go('/');
+              } else {
+                PageRouter.router.go('/statistic');
+              }
+            },
+          ),
+          Positioned(
+            // Behold the magic number!
+            left: ((ref.watch(_selectedIndex) / 4) *
+                    MediaQuery.of(context).size.width) +
+                (MediaQuery.of(context).size.width / 11) -
+                (MediaQuery.of(context).size.width / 32),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 8,
+              height: 4,
+              color: Colors.white,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.table_chart_outlined),
-              label: 'Statistic',
-            ),
-          ],
-          onTap: (index) {
-            if (index == 0) {
-              PageRouter.router.go('/');
-            } else {
-              PageRouter.router.go('/statistic');
-            }
-          },
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
