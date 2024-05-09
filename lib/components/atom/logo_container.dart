95649c1feea684ce30168c3ae3c1b182edfc8db3
izasoerya <ihza.surya@gmail.com>
2024-05-09 15:43:47 +0700

    logo class

---
+++ lib/components/atom/logo_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:stun_sync/database/models/logo_list.dart';
+
+class LogoContainer extends StatelessWidget {
+  const LogoContainer({super.key, required this.text, required this.logo});
+  final String text;
+  final LogoList logo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.symmetric(
+          vertical: BorderSide(
+            color: Color.fromRGBO(34, 87, 122, 0.2),
+            width: 1,
+          ),
+        ),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.white,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            logoList[logo]!,
+            width: 100,
+            height: 100,
+          ),
+          Text(
+            text,
+            style: const TextStyle(
+              color: Color.fromRGBO(34, 87, 122, 1),
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
