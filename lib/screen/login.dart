55cb1f9d41eb24f7f9449a243000d9bc1b57bc6f
izasoerya <ihza.surya@gmail.com>
2024-05-22 19:59:36 +0700

    db implementation

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:stun_sync/models/user_profile.dart';
 import 'package:stun_sync/router/page_router.dart';
 import 'package:stun_sync/components/atom/role_slider.dart';
 import 'package:stun_sync/components/atom/text_field_design.dart';
+import 'package:stun_sync/service/sqlite_db.dart';
+import 'package:stun_sync/service/user_profile_data.dart';
 
 bool isLoggedIn = false;
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
+  final SQLiteDB sqLiteDB = const SQLiteDB();
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  late final TextEditingController usernameController;
+  late final TextEditingController passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    usernameController = TextEditingController();
+    passwordController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +43,7 @@ class LoginPage extends StatelessWidget {
           const Text(
             'Welcome to Stun Sync',
             style: TextStyle(
-              fontFamily: 'Poppins', // Add this line
+              fontFamily: 'Poppins',
               color: Colors.white,
               fontSize: 24,
               fontWeight: FontWeight.bold,
@@ -29,29 +51,29 @@ class LoginPage extends StatelessWidget {
           ),
           const Text('Please login to continue',
               style: TextStyle(
-                fontFamily: 'Poppins', // Add this line
+                fontFamily: 'Poppins',
                 color: Color.fromRGBO(205, 205, 205, 1),
                 fontSize: 16,
               )),
           const Padding(padding: EdgeInsets.only(top: 15)),
           const RoleSlider(),
           const Padding(padding: EdgeInsets.only(top: 15)),
-          const TextFieldDesign(label: 'Username'),
+          TextFieldDesign(label: 'Username', controller: usernameController),
           const Padding(padding: EdgeInsets.only(top: 15)),
-          const TextFieldDesign(label: 'Password'),
+          TextFieldDesign(label: 'Password', controller: passwordController),
           const Padding(padding: EdgeInsets.only(top: 10)),
           const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('Didnt have an account? ',
                   style: TextStyle(
-                    fontFamily: 'Poppins', // Add this line
+                    fontFamily: 'Poppins',
                     color: Colors.white,
                     fontSize: 16,
                   )),
               Text('Register here',
                   style: TextStyle(
-                    fontFamily: 'Poppins', // Add this line
+                    fontFamily: 'Poppins',
                     color: Color.fromRGBO(128, 237, 153, 1),
                     fontSize: 16,
                   )),
@@ -61,11 +83,50 @@ class LoginPage extends StatelessWidget {
           SizedBox(
             width: 100,
             child: ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
                 isLoggedIn = true;
-                PageRouter.router.go('/');
+                Database db = await widget.sqLiteDB.openDB();
+                widget.sqLiteDB.showAllUsers(db);
+                // widget.sqLiteDB.insertUser(
+                //     db,
+                //     const UserProfile(
+                //       name: 'admin',
+                //       password: 'admin',
+                //       height: 190,
+                //       weight: 85,
+                //       age: 35,
+                //       admin: true,
+                //     ));
+                final isUserExist = await widget.sqLiteDB.searchUser(
+                    db, usernameController.text, passwordController.text);
+                if (isUserExist) {
+                  final user = UserProfile(
+                    name: usernameController.text,
+                    password: passwordController.text,
+                    height: 190,
+                    weight: 85,
+                    age: 35,
+                    admin: false,
+                  );
+                  ref.read(userProfile.notifier).setUser(user);
+                  PageRouter.router.go('/');
+                } else {
+                  final snackBar = SnackBar(
+                    elevation: 0,
+                    behavior: SnackBarBehavior.floating,
+                    backgroundColor: Colors.transparent,
+                    content: AwesomeSnackbarContent(
+                      title: 'Login Gagal',
+                      message: 'Preiksa kembali username dan password anda.',
+                      contentType: ContentType.failure,
+                    ),
+                  );
+
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(snackBar);
+                }
               },
-              child: const Text('Login'),
               style: ButtonStyle(
                 foregroundColor: MaterialStateProperty.all(
                     const Color.fromRGBO(34, 87, 122, 1)),
@@ -78,10 +139,19 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
               ),
+              child: const Text('Login'),
             ),
           ),
+          Padding(padding: EdgeInsets.only(top: 10)),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 }
---
+++ lib/service/sqlite_db.dart
@@ -0,0 +1,101 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/utils/utils.dart';
+import 'package:stun_sync/models/user_profile.dart';
+
+class SQLiteDB {
+  const SQLiteDB();
+
+  Future<String> getPathDB() async {
+    String path = await getDatabasesPath();
+    return join(path, 'stun_sync.db');
+  }
+
+  Future<bool> isTableExists(DatabaseExecutor db, String table) async {
+    var count = firstIntValue(await db.query('sqlite_master',
+        columns: ['COUNT(*)'],
+        where: 'type = ? AND name = ?',
+        whereArgs: ['table', table]));
+    return count! > 0;
+  }
+
+  Future<Database> openDB() async {
+    String path = await getPathDB();
+    Database database =
+        await openDatabase(path, version: 1, onCreate: (db, version) {
+      db.execute('''
+        CREATE TABLE user_profile(
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          password TEXT,
+          height INTEGER,
+          weight INTEGER,
+          age INTEGER,
+          admin TEXT
+        )
+      ''');
+    });
+    return database;
+  }
+
+  Future<void> showTables(Database db) async {
+    List<Map<String, dynamic>> tables = await db.rawQuery(
+      'SELECT name FROM sqlite_master WHERE type = "table"',
+    );
+
+    tables.forEach((element) {
+      print(element);
+    });
+  }
+
+  Future<void> showAllUsers(Database db) async {
+    List<Map<String, dynamic>> users = await db.query('user_profile');
+
+    users.forEach((user) {
+      print(user);
+    });
+  }
+
+  Future<void> insertUser(Database db, UserProfile user) async {
+    await db.insert('user_profile', {
+      'name': user.name,
+      'password': user.password,
+      'height': user.height,
+      'weight': user.weight,
+      'age': user.age,
+      'admin': user.admin.toString(),
+    });
+  }
+
+  Future<void> deleteUser(Database db, {int? id, String? name}) async {
+    if (id != null) {
+      await db.delete(
+        'user_profile',
+        where: 'id = ?',
+        whereArgs: [id],
+      );
+    } else if (name != null) {
+      await db.delete(
+        'user_profile',
+        where: 'name = ?',
+        whereArgs: [name],
+      );
+    } else {
+      throw ArgumentError('You must provide either id or name');
+    }
+  }
+
+  Future<bool> searchUser(Database db, String name, String password) async {
+    final List<Map<String, dynamic>> maps = await db.query(
+      'user_profile',
+      where: 'name = ? AND password = ?',
+      whereArgs: [name, password],
+    );
+
+    return maps.isNotEmpty;
+  }
+
+  Future<void> closeDB(Database db) async {
+    await db.close();
+  }
+}
