65036915bd4fffb4115252502e45642c5165435a
izasoerya <ihza.surya@gmail.com>
2024-07-12 14:58:42 +0700

    add safe area

--- lib/components/atom/top_of_bar.dart
+++ lib/components/atom/top_of_bar.dart
@@ -10,69 +10,76 @@ class TopOfBar extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.only(
-        top: 20.0,
         left: 20.0,
         right: 20.0,
       ),
-      child: Stack(
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: 75,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                const Text(
-                  'Welcome',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey,
-                  ),
-                ),
-                Flexible(
-                  child: Text(
-                    "Halo Orangtua ${ref.read(userProfileProvider).name}",
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: PopupMenuButton(
+                    icon: const Icon(
+                      Icons.settings,
                       color: Colors.white,
+                      size: 40,
                     ),
-                    overflow: TextOverflow.ellipsis,
-                    softWrap: false,
+                    color: Colors.white,
+                    offset: Offset(
+                        -30, 60), // Adjust the offset to position the menu
+                    itemBuilder: (BuildContext context) {
+                      return [
+                        PopupMenuItem(
+                          child: Text('Keluar'),
+                          value: 'logout',
+                        ),
+                      ];
+                    },
+                    onSelected: (String value) {
+                      if (value == 'logout') {
+                        // Perform logout action
+                        PageRouter.router.go('/login');
+                      }
+                    },
                   ),
                 ),
               ],
             ),
-          ),
-          Positioned(
-            top: 0,
-            right: 0,
-            child: PopupMenuButton(
-              icon: const Icon(
-                Icons.settings,
-                color: Colors.white,
-                size: 40,
-              ),
-              color: Colors.white,
-              offset: Offset(-30, 60), // Adjust the offset to position the menu
-              itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(
-                    child: Text('Keluar'),
-                    value: 'logout',
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Welcome',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
                   ),
-                ];
-              },
-              onSelected: (String value) {
-                if (value == 'logout') {
-                  // Perform logout action
-                  PageRouter.router.go('/login');
-                }
-              },
+                  Flexible(
+                    child: Text(
+                      "Halo Orangtua ${ref.read(userProfileProvider).name}",
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: false,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
